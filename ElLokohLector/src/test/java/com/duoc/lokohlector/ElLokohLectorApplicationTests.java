b82be2b78e44c7ae19aa38798e4883b2d3416aae
DEV-MIPH <104047318+DEV-MIPH@users.noreply.github.com>
2024-04-02 16:09:01 -0300

    Se inicia proyecto

---
+++ ElLokohLector/src/test/java/com/duoc/lokohlector/ElLokohLectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.lokohlector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElLokohLectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
