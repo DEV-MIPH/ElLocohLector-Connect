620a675e3edf6af35ca0ebcf0e6efecf52937efa
DEV-MIPH <104047318+DEV-MIPH@users.noreply.github.com>
2024-06-11 08:54:45 -0400

    chore: Update CORS dependency version in package.json and app.js/app.ts

--- LocoLector-Express/app.js
+++ LocoLector-Express/app.js
@@ -10,7 +10,7 @@ const app = (0, express_1.default)();
 const port = 3000;
 app.use((0, cors_1.default)({
     origin: (origin, callback) => {
-        if (origin && origin.includes('https://el-locoh-lector.vercel.app/')) {
+        if (origin && origin.includes('localhost')) {
             callback(null, true);
         }
         else {
@@ -22,14 +22,6 @@ app.get('/libros', controller_1.getAllBooksController);
 app.get('/', (req, res) => {
     res.send('¡Hola, mundo!');
 });
-const server = app.listen(port, '127.0.0.1', () => {
-    const address = server.address();
-    if (address) {
-        const host = address.address;
-        const port = address.port;
-        console.log(`Servidor corriendo en http://${host}:${port}`);
-    }
-    else {
-        console.error('No se pudo obtener la dirección del servidor');
-    }
+app.listen(port, () => {
+    console.log(`Servidor Express corriendo en http://localhost:${port}`);
 });
--- LocoLector-Express/app.ts
+++ LocoLector-Express/app.ts
@@ -1,24 +1,24 @@
 import express from 'express';
 import { getAllBooksController } from './controller/controller';
 import cors from 'cors';
-import { AddressInfo } from 'net';
 
 const app = express();
 const port = 3000;
 
 app.use(cors({
     origin: (origin, callback) => {
-        if (origin && origin.includes('https://el-locoh-lector.vercel.app/')) {
-            callback(null, true);
-        } else {
-            callback(new Error('No permitido'));
+            if (origin && origin.includes('localhost')) {
+                callback(null, true);
+            } else {
+                callback(new Error('No permitido'));
+            }
         }
     }
-}
-)
+    )
 );
 
 app.get('/libros', getAllBooksController);
+
 
 
 app.get('/', (req, res) => {
@@ -26,14 +26,8 @@ app.get('/', (req, res) => {
 });
 
 
-const server = app.listen(port, '127.0.0.1', () => {
-    const address = server.address() as AddressInfo;
-    if (address) {
-        const host = address.address;
-        const port = address.port;
-        console.log(`Servidor corriendo en http://${host}:${port}`);
-    } else {
-        console.error('No se pudo obtener la dirección del servidor');
-    }
+app.listen(port, () => {
+    console.log(`Servidor Express corriendo en http://localhost:${port}`);
 });
+
 
