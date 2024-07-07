865be17e48573a2972c6895931b297a54aa05f17
DEV-MIPH <104047318+DEV-MIPH@users.noreply.github.com>
2024-07-07 16:14:34 -0400

    chore: Update CORS configuration to allow requests from https://lokolector.vercel.app/

--- LocoLector-Express/app.ts
+++ LocoLector-Express/app.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use(cors({
     origin: (origin, callback) => {
-            if (origin && origin.includes('localhost')) {
+            if (origin && origin.includes('https://lokolector.vercel.app/')) {
                 callback(null, true);
             } else {
                 callback(new Error('No permitido'));
